52792d3f2c4e6986676da2c146432a8b64ecdd90
axel kaban <axelkaban@gmail.com>
2017-05-23 20:17:31 -0400

    media queries and em sizes

--- scripts/script.js
+++ scripts/script.js
@@ -16,29 +16,29 @@ function toScroll(element, dist) {
   });
 };
 
-toScroll('p.about', 778);
-toScroll('p.skills', 1560);
+toScroll('p.about', 770);
+toScroll('p.skills', 1590);
 toScroll('p.projects', 2420);
 toScroll('p.interests', 3200);
-toScroll('p.contact', 4100);
-toScroll('p.resume', 4878);
+toScroll('p.contact', 4025);
+toScroll('p.resume', 4820);
 
-// if (window.scroll(0, 778)){
-//     document.querySelector('p.about').setAttribute('style', 'background-color: #00b4ff;');
-// }
-// else if (typeof(window.pageYOffset) === 1560){
-//     document.querySelector('p.skills').setAttribute('style', 'background-color: #00b4ff;');
-// }
-// else if (typeof(window.pageYOffset) === 2420){
-//     document.querySelector('p.projects').setAttribute('style', 'background-color: #00b4ff;');
-// }
-// else if (typeof(window.pageYOffset) === 3200){
-//     document.querySelector('p.interests').setAttribute('style', 'background-color: #00b4ff;');
-// } 
-// else if (typeof(window.pageYOffset) === 4100){
-//     document.querySelector('p.contact').setAttribute('style', 'background-color: #00b4ff;');
-// }
-// else if(typeof(window.pageYOffset) === 4878){
-//     document.querySelector('p.resume').setAttribute('style', 'background-color: #00b4ff;');
-// }
+if (window.scroll(0, 778)){
+    document.querySelector('p.about').setAttribute('style', 'background-color: #00b4ff;');
+}
+else if (window.pageYOffset === 1560){
+    document.querySelector('p.skills').setAttribute('style', 'background-color: #00b4ff;');
+}
+else if (window.pageYOffset === 2420){
+    document.querySelector('p.projects').setAttribute('style', 'background-color: #00b4ff;');
+}
+else if (window.pageYOffset === 3200){
+    document.querySelector('p.interests').setAttribute('style', 'background-color: #00b4ff;');
+} 
+else if (window.pageYOffset === 4100){
+    document.querySelector('p.contact').setAttribute('style', 'background-color: #00b4ff;');
+}
+else if(window.pageYOffset === 4878){
+    document.querySelector('p.resume').setAttribute('style', 'background-color: #00b4ff;');
+}
  
