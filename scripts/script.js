e22c8172c6d5fba628000027f6649877cee6401e
axel kaban <y>
2017-06-07 13:58:12 -0400

    fixes on the nav bar and adjustments to go along with them. set a hover animation on the intro button for further clearing up the need to click

--- scripts/script.js
+++ scripts/script.js
@@ -10,18 +10,18 @@ button.addEventListener('click', ()=>{
   
 });
 
-function toScroll(element, dist) {
-  let el = document.querySelector(element);
-  return el.addEventListener('click', () => {
-    window.scroll(0, dist);
-  });
-};
+// function toScroll(element, dist) {
+//   let el = document.querySelector(element);
+//   return el.addEventListener('click', () => {
+//     window.scroll(0, dist);
+//   });
+// };
 
-toScroll('p.about', document.querySelector('div.about').getBoundingClientRect().top+window.scrollY - document.querySelector('nav').offsetHeight + .38);
-toScroll('p.skills', document.querySelector('div.skills').getBoundingClientRect().top+window.scrollY - document.querySelector('nav').offsetHeight + .38);
-toScroll('p.projects', document.querySelector('div.projects').getBoundingClientRect().top+window.scrollY - document.querySelector('nav').offsetHeight + .38);
-toScroll('p.interests', document.querySelector('div.interests').getBoundingClientRect().top+window.scrollY - document.querySelector('nav').offsetHeight + .38);
-toScroll('p.contact', document.querySelector('div.contact').getBoundingClientRect().top+window.scrollY);
-toScroll('p.resume', document.querySelector('div.resume').getBoundingClientRect().top+window.scrollY - document.querySelector('nav').offsetHeight + .38);
+// toScroll('p.about', document.querySelector('div.about').getBoundingClientRect().top+window.scrollY - document.querySelector('nav').offsetHeight + .38);
+// toScroll('p.skills', document.querySelector('div.skills').getBoundingClientRect().top+window.scrollY - document.querySelector('nav').offsetHeight + .38);
+// toScroll('p.projects', document.querySelector('div.projects').getBoundingClientRect().top+window.scrollY - document.querySelector('nav').offsetHeight + .38);
+// toScroll('p.interests', document.querySelector('div.interests').getBoundingClientRect().top+window.scrollY - document.querySelector('nav').offsetHeight + .38);
+// toScroll('p.contact', document.querySelector('div.contact').getBoundingClientRect().top+window.scrollY);
+// toScroll('p.resume', document.querySelector('div.resume').getBoundingClientRect().top+window.scrollY - document.querySelector('nav').offsetHeight + .38);
 
  
