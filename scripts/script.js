c888136de2dcd26ea6a562ec471e88ec506ef73c
axel kaban <y>
2017-06-15 00:00:22 -0400

    edits on how to pass the intro page and resizing of images

--- scripts/script.js
+++ scripts/script.js
@@ -2,15 +2,20 @@ button = document.querySelector("button");
 body = document.querySelector("body");
 panels = document.querySelector("div.panel-container");
 
-button.addEventListener('click', ()=>{
-   body.classList.toggle('open');
+button.addEventListener('mouseover', (e)=>{
+ setTimeout(function(){
+  body.classList.add('open');
    body.setAttribute('style', 'overflow: scroll;');
    button.setAttribute('style', 'z-index: 0;');
    window.scroll(0,0);
 
    setTimeout(function(){ panels.setAttribute('style', 'visibility:hidden;'), 1000 });
   
+},1100);
+   
 });
+
+
 
 
 
@@ -19,14 +24,14 @@ function toScroll(element, dist) {
   return el.addEventListener('click', (e) => {
     window.location.href = dist;
     history.pushState('', document.title, window.location.pathname); // nice and clean
-    e.preventDefault(); // no page reload
+    // e.preventDefault(); // no page reload
   });
 };
 
 toScroll('a.about', "#about");
 toScroll('a.skills', "#skills");
 toScroll('a.projects', "#projects");
-toScroll('a.interests', "#interests");
+// toScroll('a.interests', "#interests");
 toScroll('a.contact', "#contact");
 toScroll('a.resume', "#resume");
 
